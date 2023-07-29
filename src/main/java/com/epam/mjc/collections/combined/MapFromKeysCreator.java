d349987884aad0f29f8e490c17102c3f04646870
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-29 10:00:54 +0400

    [main] Filled up the MapFromKeysCreator.java

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.collections.combined;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
-        return new HashMap<>();
+        HashMap<Integer, Set<String>> map = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            int length = entry.getKey().length();
+            Set<String> set = map.get(length);
+            if (set == null) {
+                set = new HashSet<>();
+                map.put(length, set);
+            }
+            set.add(entry.getKey());
+        }
+
+        return map;
     }
 }
